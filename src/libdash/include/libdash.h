6c33a229f5fa205b612ee59878c2ee6fd7b3dae3
Richard Moseley <richard.moseley4@gmail.com>
2023-07-10 14:05:23 +0100

    Move the header file for the libdash library

--- src/libdash/include/libdash.h
+++ src/libdash/include/libdash.h
@@ -7,8 +7,12 @@
 #include <stdbool.h>
 
 /* Provide opaque types to library structures and unions */
-union parse_node;
 struct parse_context;
+#ifdef USE_WALKINFO
+struct parse_walkinfo;
+#else
+union parse_node;
+#endif
 
 /* Provide the interface to the context used by the library */
 bool parse_new(struct parse_context **);
@@ -16,5 +20,13 @@ void parse_free(struct parse_context **);
 const char *parse_internal_errstr(struct parse_context *);
 bool parse_push_string(struct parse_context *, const char *);
 bool parse_push_file(struct parse_context *, const char *);
+#ifdef USE_WALKINFO
+bool parse_iseof(struct parse_walkinfo *);
+struct parse_walkinfo *parse_next_command(struct parse_context *);
+bool parse_walk_next(struct parse_walkinfo *);
+bool parse_walk_free(struct parse_walkinfo **);
+int parse_walk_type(struct parse_walkinfo *);
+#else
+bool parse_iseof(union parse_node *);
 union parse_node *parse_next_command(struct parse_context *);
-bool parse_node_iseof(union parse_node *);
+#endif
